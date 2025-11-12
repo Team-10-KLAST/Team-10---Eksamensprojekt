9fbdb1616d45797389c4c0b3544aaed1ba056e54
Annette Jensen <annetteduedal@gmail.com>
2025-11-12 10:10:12 +0100

    DatabaseConnection tilføjet

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public sealed class DatabaseConnection
+    {
+        private static DatabaseConnection _instance;
+
+        private readonly string _connectionString;
+        private DatabaseConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static void Instance(string connectionString)
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseConnection(connectionString);
+            }
+            else
+            {
+                throw new InvalidOperationException("DatabaseConnection is already initialized.");
+            }
+        }
+
+        public static DatabaseConnection GetInstance()
+        {
+            if (_instance == null)
+            {
+                throw new InvalidOperationException("DatabaseConnection is not initialized. Call Instance() first.");
+            }
+            return _instance;
+        }
+    }
+}
--- Presentation.Wpf/App.xaml.cs
+++ Presentation.Wpf/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Data;
 
 namespace Presentation.Wpf
 {
@@ -9,6 +12,21 @@ namespace Presentation.Wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            DatabaseConnection.Instance(connectionString);
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 
 }
