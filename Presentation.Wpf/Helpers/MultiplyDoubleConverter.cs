324682fa746e13c5850367e69a234ad2309437a6
Annette Jensen <annetteduedal@gmail.com>
2025-11-17 09:30:56 +0100

    employeeview+viewmodel

---
+++ Presentation.Wpf/Helpers/MultiplyDoubleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Presentation.Wpf.Helpers
+{
+    public class MultiplyDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double input && double.TryParse(parameter?.ToString(), out double factor))
+                return input * factor;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Presentation.Wpf/ViewModels/OverlayHostViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Presentation.Wpf.Commands;
+
+namespace Presentation.Wpf.ViewModels
+{
+    // ViewModel for managing overlay panels in the application. It handles displaying and closing overlays.
+    public class OverlayHostViewModel : ViewModelBase
+    {
+        // The currently displayed overlay panel. Null if no overlay is shown.
+        private object? _currentOverlay;
+        public object? CurrentOverlay
+        {
+            get => _currentOverlay;
+            set
+            {
+                if (SetProperty(ref _currentOverlay, value))
+                    IsOverlayOpen = value != null;
+            }
+        }
+
+        // Indicates whether an overlay panel is currently open. Used for controlling overlay visibility.
+        private bool _isOverlayOpen;
+        public bool IsOverlayOpen
+        {
+            get => _isOverlayOpen;
+            set => SetProperty(ref _isOverlayOpen, value);
+        }
+
+        // Command to close the currently displayed overlay panel.
+        public ICommand CloseOverlayCommand { get; }
+
+        // Constructor initializing the CloseOverlayCommand.
+        public OverlayHostViewModel()
+        {
+            CloseOverlayCommand = new RelayCommand(() => CurrentOverlay = null);
+        }
+
+        // Method to show a new overlay panel. Subscribes to the overlay's RequestClose event, so it can be closed from within.
+        public void ShowOverlay(OverlayPanelViewModelBase overlay)
+        {
+            overlay.RequestClose += (sender, eventArgs) => CurrentOverlay = null;
+            CurrentOverlay = overlay;
+        }
+    }
+}
