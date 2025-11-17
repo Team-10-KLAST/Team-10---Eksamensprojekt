e02f2f2ff64a1516e239cca5e7a62e51ee555c94
Tai Ngo <tdng71414@edu.ucl.dk>
2025-11-17 02:04:03 +0100

    Implemented AddEmployee

---
+++ Application/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace Application.Models
+{
+    public enum Department
+    {
+        Engineering,
+        Sales,
+        Accounting,
+        Administration
+    }
+
+    public enum Role
+    {
+        Manager,
+        Employee
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public Department Department { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
