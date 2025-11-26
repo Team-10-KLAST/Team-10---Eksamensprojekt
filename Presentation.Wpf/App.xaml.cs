5222b2e4d528640821daa3fc39529ac5f6b99f52
Annette Jensen <annetteduedal@gmail.com>
2025-11-26 12:04:37 +0100

    fejl fixet

--- Presentation.Wpf/App.xaml.cs
+++ Presentation.Wpf/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Presentation.Wpf
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
