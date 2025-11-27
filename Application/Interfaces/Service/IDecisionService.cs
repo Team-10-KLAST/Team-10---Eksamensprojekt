7d847f10816e15cfddfbc7f8b88d173275120d2f
Sabine Jensen <ssje71367@edu.ucl.dk>
2025-11-27 16:58:04 +0100

    Added IDecisionService - interface

---
+++ Application/Interfaces/Service/IDecisionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+
+namespace Application.Interfaces.Service;
+
+public interface IDecisionService
+{
+    //Decision GetDecisionByID(int decisionID);
+    void ApproveLoan(int loanID, string comment);
+    void RejectLoan(int loanID, string comment);
+}
