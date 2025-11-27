5d4931d0c28c94e256cd0b9bef5a5f313b95ada1
Sabine Jensen <ssje71367@edu.ucl.dk>
2025-11-27 17:07:32 +0100

    Added DecisionService with empty methods from dcd

---
+++ Application/Services/DecisionService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.Repository;
+using Application.Interfaces.Service;
+using Application.Models;
+
+namespace Application.Services;
+
+public class DecisionService : IDecisionService
+{
+
+
+    public DecisionService()
+    { }
+
+    /*public Decision GetDecisionByID(int id)
+    { } */
+
+    public void ApproveLoan(int id, string comment)
+    { }
+
+    public void RejectLoan(int id, string comment)
+    { }
+
+}
