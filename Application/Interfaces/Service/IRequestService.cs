ec1f88ab7116f117932a92b6f3cafe573e6ca9d5
Sabine Jensen <ssje71367@edu.ucl.dk>
2025-11-27 17:14:54 +0100

    added IRequestService interface with methods from dcd

---
+++ Application/Interfaces/Service/IRequestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+
+namespace Application.Interfaces.Service
+{
+    public interface IRequestService
+    {
+        Request GetRequestByID(int requestID);
+        IEnumerable<Request> GetAllRequests();
+        public void SubmitRequest(Request request);
+        public void CancelRequest(int requestID);
+        public void UpdateRequestStatus(int requestID, string newStatus);
+    }
+}
