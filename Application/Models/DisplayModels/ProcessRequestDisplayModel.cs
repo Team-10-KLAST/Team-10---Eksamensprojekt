387e2cea0096ef9276b60ba6573b5e8ec339f0b7
Annette Jensen <annetteduedal@gmail.com>
2025-12-02 14:44:02 +0100

    ProcessRequestViewModel + metoder i RequestService

---
+++ Application/Models/DisplayModels/ProcessRequestDisplayModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.DisplayModels
+{
+    public class ProcessRequestDisplayModel
+    {
+        public string TentativeAssigneeEmail { get; set; } = "";
+        public string DeviceType { get; set; } = "";
+        public string OperatingSystem { get; set; } = "";
+        public string Location { get; set; } = "";
+    }
+}
