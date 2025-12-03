cefd0c41a9a0a34ac67c9a35da9ac94ae439e5d7
Tai Ngo <tdng71414@edu.ucl.dk>
2025-12-03 14:59:14 +0100

    Oprettet UnitTestProjekt

---
+++ AssetManagerTest/DecisionServiceTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application.Models;
+using Application.Interfaces;
+using Application.Services;
+
+namespace AssetManagerTest;
+
+[TestClass]
+public class DecisionServiceTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
