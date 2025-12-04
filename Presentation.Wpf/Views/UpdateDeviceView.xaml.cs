f01294d26148a567e4636be4c817543db02b9a2b
Krislindby <kristinalindby1994@gmail.com>
2025-12-04 10:05:58 +0100

    NEW UpdateDevice View+ViewModel

--- Presentation.Wpf/Views/UpdateDeviceView.xaml.cs
+++ Presentation.Wpf/Views/UpdateDeviceView.xaml.cs
@@ -21,5 +21,10 @@ namespace Presentation.Wpf.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
