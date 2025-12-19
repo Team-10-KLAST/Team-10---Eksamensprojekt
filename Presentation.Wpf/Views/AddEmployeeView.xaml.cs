e8bc2775da1dab2200a2ce8e7165e5f2f607d3ed
Tai Ngo <tdng71414@edu.ucl.dk>
2025-12-19 13:56:52 +0100

    fjernet alle codebehind

--- Presentation.Wpf/Views/AddEmployeeView.xaml.cs
+++ Presentation.Wpf/Views/AddEmployeeView.xaml.cs
@@ -23,10 +23,5 @@ namespace Presentation.Wpf.Views
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
